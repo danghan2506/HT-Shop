b6179d71ea80a83ac6eaa4201af170f6ecd01eeb
danghan2506 <danghan1213@gmail.com>
2025-07-05 21:20:01 +0700

    Backend|added category routes and controllers

--- backend/controllers/category-controllers.js
+++ backend/controllers/category-controllers.js
@@ -1,27 +1,81 @@
-
 import asyncHandler from "../middlewares/async-handler.js";
 import { Category } from "../models/category-model.js";
-const createCategory = asyncHandler(async(req, res) => {
-    const {name} = req.body
-    if(!name){
-        throw new Error("Please enter category's name")
+const createCategory = asyncHandler(async (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    throw new Error("Please enter category's name");
+  }
+  const existedCategory = await Category.findOne({ name });
+  if (existedCategory) {
+    res.status(404).send("Cateogy already existed!");
+    return;
+  }
+  const newCategory = new Category({ name });
+  try {
+    await newCategory.save();
+    res.status(201).json({ newCategory });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid category");
+  }
+});
+const updateCategory = asyncHandler(async (req, res) => {
+  const { categoryId } = req.params;
+  try {
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      res.status(404);
+      throw new Error("ID not found!");
+    } else {
+      category.name = req.body.name || category.name;
+      const updateCategory = await category.save();
+      res.status(200);
+      res.json({ updateCategory });
     }
-    const existedCategory = await Category.findOne({name})
-    if(existedCategory){
-        res.status(404).send("Cateogy already existed!")
-        return
-    }
-    const newCategory = new Category({name})
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+const deleteCategory = asyncHandler(async(req, res) => {
+    const {categoryId} = req.params
     try {
-        await newCategory.save()
-        res.status(201).json({
-            _id: newCategory._id,
-            name: newCategory.name
-        })
+        const category = await Category.findByIdAndDelete(categoryId)
+        if(!category){
+            res.status(401)
+            throw new Error("Cannot delete this category")
+        }
+        else{
+            res.status(201)
+            res.json("Delete category successfully!")
+        }
     } catch (error) {
-        res.status(400)
-        throw new Error("Invalid category")
+        throw new Error("Internal server error")
     }
-
 })
-export {createCategory}
+const listCategory = asyncHandler(async(req, res) => {
+    try {
+        const listCategory = await Category.find()
+        if(!listCategory){
+            res.status(401)
+            res.json("No category found!")
+        }
+        res.json({listCategory})
+    } catch (error) {
+        throw new Error("Internal server error")
+    }
+})
+const readCategory = asyncHandler(async(req, res) => {
+    const {categoryId} = req.params
+    try {
+        const category = await Category.findById(categoryId)
+        if(!category){
+            res.status(404)
+            throw new Error("No category found")
+        }
+        res.json(category)
+    } catch (error) {
+        throw new Error("Internal server error")
+    }
+})
+export { createCategory, updateCategory, deleteCategory, listCategory , readCategory};
--- backend/routes/category-routes.js
+++ backend/routes/category-routes.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { authenticate, authorizedAdmin } from "../middlewares/auth-middleware.js";
-import {createCategory} from "../controllers/category-controllers.js"
+import {createCategory, updateCategory, deleteCategory, listCategory, readCategory} from "../controllers/category-controllers.js"
 const router = express.Router()
-router.route("/").get(authenticate, authorizedAdmin).post(createCategory)
+router.route("/").post(authenticate, authorizedAdmin, createCategory)
+router.route("/:categoryId").put(authenticate,authorizedAdmin, updateCategory)
+router.route("/:categoryId").delete(authenticate, authorizedAdmin, deleteCategory)
+router.route("/categories").get(listCategory)
+router.route("/:categoryId").get(readCategory)
 export default router
