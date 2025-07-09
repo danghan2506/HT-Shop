f8c678653b7b07c36e0f3792865f641d1a6a5869
danghan2506 <danghan1213@gmail.com>
2025-07-09 21:51:40 +0700

    Frontend| Completed AdminMenu

--- frontend/src/components/admin-menu.jsx
+++ frontend/src/components/admin-menu.jsx
@@ -1,105 +1,50 @@
-import React, {useState}from 'react'
-import { NavLink } from "react-router-dom";
-import { FaTimes } from "react-icons/fa";
+import React from "react";
+import { AlignJustify, X} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuGroup
+} from "@/components/ui/dropdown-menu";
+import { Link } from "react-router-dom";
+import {Button} from "@/components/ui/button";
 const AdminMenu = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
   return (
-    <>
-      <button
-        className={`${
-          isMenuOpen ? "top-2 right-2" : "top-5 right-7"
-        } bg-[#151515] p-2 fixed rounded-lg`}
-        onClick={toggleMenu}
-      >
-        {isMenuOpen ? (
-          <FaTimes color="white" />
-        ) : (
-          <>
-            <div className="w-6 h-0.5 bg-gray-200 my-1"></div>
-            <div className="w-6 h-0.5 bg-gray-200 my-1"></div>
-            <div className="w-6 h-0.5 bg-gray-200 my-1"></div>
-          </>
-        )}
-      </button>
-
-      {isMenuOpen && (
-        <section className="bg-[#151515] p-4 fixed right-7 top-5">
-          <ul className="list-none mt-2">
-            <li>
-              <NavLink
-                className="py-2 px-3 block mb-5 hover:bg-[#2E2D2D] rounded-sm"
-                to="/admin/dashboard"
-                style={({ isActive }) => ({
-                  color: isActive ? "greenyellow" : "white",
-                })}
-              >
-                Admin Dashboard
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className="py-2 px-3 block mb-5 hover:bg-[#2E2D2D] rounded-sm"
-                to="/admin/category"
-                style={({ isActive }) => ({
-                  color: isActive ? "greenyellow" : "white",
-                })}
-              >
-                Create Category
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className="py-2 px-3 block mb-5 hover:bg-[#2E2D2D] rounded-sm"
-                to="/admin/create-products"
-                style={({ isActive }) => ({
-                  color: isActive ? "greenyellow" : "white",
-                })}
-              >
-                Create Product
-              </NavLink>
-            </li>
-            {/* <li>
-              <NavLink
-                className="py-2 px-3 block mb-5 hover:bg-[#2E2D2D] rounded-sm"
-                to="/admin/allproductslist"
-                style={({ isActive }) => ({
-                  color: isActive ? "greenyellow" : "white",
-                })}
-              >
-                All Products
-              </NavLink>
-            </li> */}
-            <li>
-              <NavLink
-                className="py-2 px-3 block mb-5 hover:bg-[#2E2D2D] rounded-sm"
-                to="/admin/userlist"
-                style={({ isActive }) => ({
-                  color: isActive ? "greenyellow" : "white",
-                })}
-              >
-                Manage Users
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className="py-2 px-3 block mb-5 hover:bg-[#2E2D2D] rounded-sm"
-                to="/admin/orderlist"
-                style={({ isActive }) => ({
-                  color: isActive ? "greenyellow" : "white",
-                })}
-              >
-                Manage Orders
-              </NavLink>
-            </li>
-          </ul>
-        </section>
-      )}
-    </>
+        <div className="fixed top-4 right-6 z-50">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="bg-neutral-400 hover:bg-neutral-700 text-white shadow-md rounded-full p-2">
+                <AlignJustify className="h-5 w-5" />
+              </Button>
+            </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56 mt-2 bg-white border border-gray-200 shadow-lg rounded-lg" align="end">
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+            <Link to="/admin/dashboard">
+              <span>Dashboard</span>
+            </Link>
+          </DropdownMenuItem>
+           <DropdownMenuItem>
+            <Link to="/admin/users-list">
+              <span>Users</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link to="/admin/category">
+              <span>Category</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link to="/admin/create-products">
+              <span>Create product</span>
+            </Link>
+          </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
   );
-}
+};
 
-export default AdminMenu
+export default AdminMenu;
